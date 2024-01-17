0a14494ca8b779f91b9fee35bff27e4be5d08cb9
Rimsha Wazir <rimshawazir48@gmail.com>
2024-01-17 10:40:11 +0500

    using bloc selector

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:dummy/Model/dummy_model.dart';
 import 'package:dummy/Repository/dummy_repo.dart';
 import 'package:dummy/cubit/dummy_cubit.dart';
 
@@ -51,76 +52,83 @@ class _CategoryListViewState extends State<CategoryListView> {
       appBar: AppBar(
         title: const Text('Product Categories Task'),
       ),
-      body: BlocConsumer<DummyCubit, DummyState>(
-        listener: (context, state) {
-          if (state is CategoryLoading) {
-            showDialog(
-              context: context,
-              barrierDismissible: false,
-              builder: (BuildContext context) {
-                return const AlertDialog(
-                  content: CircularProgressIndicator(),
-                );
-              },
-            );
-          }
+      body: BlocConsumer<DummyCubit, DummyState>(listener: (context, state) {
+        if (state is CategoryLoading) {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (BuildContext context) {
+              return const AlertDialog(
+                content: CircularProgressIndicator(),
+              );
+            },
+          );
+        }
 
-          if (state is CategoryError) {
-            showDialog(
-              context: context,
-              barrierDismissible: false,
-              builder: (BuildContext context) {
-                return const AlertDialog(
-                  title: Text('Error'),
-                  content: Text('An error occurred'),
-                );
-              },
-            );
-          } else {
-            if (Navigator.of(context).canPop()) {
-              Navigator.of(context).pop();
+        if (state is CategoryError) {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (BuildContext context) {
+              return const AlertDialog(
+                title: Text('Error'),
+                content: Text('An error occurred'),
+              );
+            },
+          );
+        } else {
+          if (Navigator.of(context).canPop()) {
+            Navigator.of(context).pop();
+          }
+        }
+      }, builder: (context, state) {
+        return BlocSelector<DummyCubit, DummyState, List<DummyModel>>(
+          selector: (state) {
+            if (state is CategoryLoaded) {
+              return state.categories;
             }
-          }
-        },
-        builder: (context, state) {
-          if (state is CategoryLoaded) {
-            return ListView.builder(
-              itemCount: state.categories.length,
-              itemBuilder: (context, index) {
-                var category = state.categories[index];
-                return ExpansionTile(
-                  title: Text(category.title!),
-                  children: category.subCategories!.map((subCategory) {
-                    return ExpansionTile(
-                      title: Text(subCategory.title!),
-                      children: subCategory.childSubCategories!
-                          .map((childSubCategory) {
-                        return ExpansionTile(
-                          title: Text(childSubCategory.title!),
-                          children: childSubCategory.deepChildSubCategory!
-                              .map((deepChild) {
-                            return ExpansionTile(
-                              title: Text(deepChild.title!),
-                              children: deepChild.products!.map((product) {
-                                return ListTile(
-                                  title: Text(product.productName!),
-                                  subtitle: Text('Price: ${product.price}'),
-                                );
-                              }).toList(),
-                            );
-                          }).toList(),
-                        );
-                      }).toList(),
-                    );
-                  }).toList(),
-                );
-              },
-            );
-          } else {
-            return const Center(child: Text('No data available'));
-          }
-        },
-      ),
+            return [];
+          },
+          builder: (context, categories) {
+            if (state is CategoryLoaded) {
+              return ListView.builder(
+                itemCount: state.categories.length,
+                itemBuilder: (context, index) {
+                  var category = state.categories[index];
+                  return ExpansionTile(
+                    title: Text(category.title!),
+                    children: category.subCategories!.map((subCategory) {
+                      return ExpansionTile(
+                        title: Text(subCategory.title!),
+                        children: subCategory.childSubCategories!
+                            .map((childSubCategory) {
+                          return ExpansionTile(
+                            title: Text(childSubCategory.title!),
+                            children: childSubCategory.deepChildSubCategory!
+                                .map((deepChild) {
+                              return ExpansionTile(
+                                title: Text(deepChild.title!),
+                                children: deepChild.products!.map((product) {
+                                  return ListTile(
+                                    title: Text(product.productName!),
+                                    subtitle: Text('Price: ${product.price}'),
+                                  );
+                                }).toList(),
+                              );
+                            }).toList(),
+                          );
+                        }).toList(),
+                      );
+                    }).toList(),
+                  );
+                },
+              );
+            } else {
+              return const Center(child: Text('No data available'));
+            }
+          },
+        );
+      }),
     );
   }
 }
